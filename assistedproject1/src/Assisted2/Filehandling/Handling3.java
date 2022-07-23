225e181d5c94c791fb86ea8a667b1deaf35ede5a
jagan151 <1999jaganreddy@gmail.com>
2022-07-23 17:23:05 +0530

    assisted

---
+++ assistedproject1/src/Assisted2/Filehandling/Handling3.java
@@ -0,0 +1,18 @@
+package Filehandling;
+
+public class Handling3 {
+static void waitFor() {
+		
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+
+	public static void main(String[] args) {
+		waitFor();
+	}
+}

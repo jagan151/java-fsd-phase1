bf5319a7926105663d0411b39b117e9bbeeacfe3
jagan151 <1999jaganreddy@gmail.com>
2022-08-13 11:14:28 +0530

    P2

---
+++ Product Details/src/main/java/com/ProductClass.java
@@ -0,0 +1,68 @@
+package com;
+
+import java.util.ArrayList;
+
+public class ProductClass {
+	private int id;
+	private String name;
+	private String color;
+	private float price;
+	
+	public ProductClass(int id, String name, String color, float price) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.color = color;
+		this.price = price;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public float getPrice() {
+		return price;
+	}
+
+	public void setPrice(float price) {
+		this.price = price;
+	}
+
+	public ArrayList<ProductClass> getProductDetails()
+	{
+		ArrayList<ProductClass> productList = new ArrayList<ProductClass>();
+		 
+		productList.add(new ProductClass(id, name, color, price));  
+		 
+		 for (ProductClass s : productList) 
+			{
+				System.out.print("ID, Name, Color, and Price of the product are : ");
+				System.out.println(s.id + " " + s.name + " " + s.color + " " + s.price);
+			}
+		return productList;
+	}
+}
+
+
+
+
